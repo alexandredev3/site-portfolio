cef3e904dbb6ba40682ecd3ae645303031dad973
Alexandremotion <costasalexandre511@gmail.com>
2020-05-07 20:54:00 -0300

    completed project

---
+++ modulo03/src/data.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    id: 'OyTPNNIy3pc',
+    title: 'Debounce do zero com javascript | Mayk Brito',
+    duration: '32:00',
+    price: 'free',
+    featured: true
+  },
+  {
+    id: 'BwwOu29K6mE',
+    title: 'Transição de imagens com CSS3 e JavaScript puro | Mayk Brito',
+    duration: '17:27',
+    price: 'free',
+    featured: true
+  },
+  {
+    id: 'DiXbJL3iWVs',
+    title: 'Iniciando com node | Mayk Brito',
+    duration: '1:30:00',
+    price: 'free'
+  },
+  {
+    id: 'H91DhKPjhPk',
+    title: 'Responsividade na Prática | Mayk Brito',
+    duration: '1:30:00',
+    price: 'free'
+  },
+  {
+    id: 'GykTLqODQuU',
+    title: 'Formulário Animado com JS puro e CSS Animation | Mayk Brito',
+    duration: '57:56',
+    price: 'free',
+    featured: true
+  },
+  {
+    id: 'HN1UjzRSdBk',
+    title: 'Desvendando o CSS Grid na prática | Mayk Brito',
+    duration: '30:16',
+    price: 'free'
+  }
+] 
